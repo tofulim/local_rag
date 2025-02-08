e906a35f886e88c3c3772f257b2d9d32ac8e7cdb
vail131 <vail131@naver.com>
2025-02-08 14:14:40 +0900

    [#3] fix crawlers to make each url parse and get info

--- crawler/blog_crawler.py
+++ crawler/blog_crawler.py
@@ -3,30 +3,43 @@ from bs4 import BeautifulSoup
 
 
 class BlogCrawler:
-    def __init__(self, url):
-        headers = {"User-Agent": "Mozilla/5.0"}
-        http = urllib3.PoolManager()  # PoolManager 객체 생성
-        response = http.request("GET", url, headers=headers)  # GET 요청 보내기
+    """
+    블로그 크롤러
+    주어진 url을 bs4로 html 파싱해 meta 데이터들을 가져온다.
+    """
+    def __init__(self):
+        self.headers = {"User-Agent": "Mozilla/5.0"}
+        self.pool_manager = urllib3.PoolManager()  # PoolManager 객체 생성
+
+    def parse(self, url: str):
+        response = self.pool_manager.request("GET", url, headers=self.headers)  # GET 요청 보내기
 
         # 응답 데이터를 파싱
-        self._soup = BeautifulSoup(response.data, "html.parser")
+        _soup = BeautifulSoup(response.data, "html.parser")
 
-    @property
-    def info(self):
+        return _soup
+
+    def get_info(self, _soup: object):
         return {
-            "title": self.title,
-            "description": self.description,
-            "image": self.image,
+            "title": self.get_title(_soup),
+            "description": self.get_description(_soup),
+            "image": self.get_image(_soup),
         }
 
-    @property
-    def title(self):
-        return self._soup.find("meta", property="og:title")["content"]
+    def get_title(self, _soup: object):
+        try:
+            return _soup.find("meta", property="og:title")["content"]
+        except Exception:
+            return None
 
-    @property
-    def description(self):
-        return self._soup.find("meta", property="og:description")["content"]
+    def get_description(self, _soup: object):
+        try:
+            return _soup.find("meta", property="og:description")["content"]
+        except Exception:
+            return None
 
-    @property
-    def image(self):
-        return self._soup.find("meta", property="og:image")["content"]
+    def get_image(self, _soup: object):
+        try:
+            return _soup.find("meta", property="og:image")["content"]
+        except Exception:
+            return None
