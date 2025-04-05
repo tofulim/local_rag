ae4df3cd97e8deac3adfbdcfe2dced11f2105ac3
vail131 <vail131@naver.com>
2025-04-05 16:01:03 +0900

    [#12] add ref docs ui and update default values

--- gradio_ui.py
+++ gradio_ui.py
@@ -1,17 +1,24 @@
 import gradio as gr
 from local_rag import LocalRAG
-from crawler.medium_crawler import MediumCrawler
-from db.vector_db import VectorDB
 from embedding.text_embedding import Vectorizer
-from language_model.llm import LanguageModel
+from crawler.crawler_factory import CrawlerFactory
+from language_model.model_factory import ModelFactory
 from summary.summarize import Summarizer
+from db.vector_db import VectorDB
 
 
-crawler = MediumCrawler()
-vector_db = VectorDB()
-vectorizer = Vectorizer()
-summarizer = Summarizer()
-llm = LanguageModel(model_name="deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B", max_new_tokens=128)
+vectorizer = Vectorizer(
+    model_name="intfloat/multilingual-e5-small",
+)
+vector_db = VectorDB(dim=vectorizer.embedding_dim)
+
+summarizer = Summarizer(model_name="digit82/kobart-summarization")
+crawler = CrawlerFactory().get("tistory")()
+llm = ModelFactory().get("kanana")(
+    model_name="kakaocorp/kanana-nano-2.1b-instruct",
+    max_new_tokens=256,
+    max_model_len=4096,
+)
 
 local_rag = LocalRAG(
     crawler=crawler,
@@ -26,7 +33,7 @@ def chatbot(question: str, history: list = []):
     # 주제 선택
     local_rag.set_rag_background(topic=question)
     # 쿼리
-    response = local_rag(
+    response, ref_docs = local_rag(
         query=question,
         num_docs=3,
     )
@@ -34,12 +41,15 @@ def chatbot(question: str, history: list = []):
 
     history.append({"role": "user", "content": question})
     history.append({"role": "assistant", "content": model_output})
-    return history
+    return history, '\n'.join(ref_docs)
 
 iface = gr.Interface(
     fn=chatbot,
-    inputs=gr.Textbox(placeholder="질문을 입력하세요"),
-    outputs=gr.Chatbot(type="messages")  # type="messages" 추가
+    inputs=gr.Textbox(label="질문", placeholder="질문을 입력하세요"),
+    outputs=[
+        gr.Chatbot(label="채팅", type="messages"),
+        gr.Textbox(label="참고 자료", interactive=False),
+    ],  # type="messages" 추가
 )
 
 if __name__ == "__main__":
